bab6441601618c382894794bb3435f0fd2fb1f9d
jigglor <andrewtsuji@gmail.com>
2017-04-20 17:58:46 -0700

    completed most of homework

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var fs = require('fs');
 var ejsLayouts = require("express-ejs-layouts");
 var bodyParser = require('body-parser');
+var db = require('./models');
 
 var app = express();
 
@@ -24,9 +25,11 @@ app.get('/', function(req, res) {
 });
 
 app.get('/games', function(req, res) {
-    var games = getGames();
-
-    res.render('games-index', { games: games });
+    db.game.findAll().then(function(games) {
+        res.render('games-index', { games: games });
+    }).catch(function(err) {
+        res.status(404).send(err);
+    });
 });
 
 app.get('/games/new', function(req, res) {
@@ -34,87 +37,62 @@ app.get('/games/new', function(req, res) {
 });
 
 app.post('/games', function(req, res) {
-    console.log(req.body);
-    var newGame = req.body;
-
-    var games = getGames();
-    games.push(newGame);
-    saveGames(games);
-
-    res.redirect('/games');
+    db.game.findOrCreate({
+        where: {
+            name: req.body.name,
+        },
+        defaults: {
+            description: req.body.description
+        }
+    }).then(function() {
+        res.redirect('/games');
+    });
 });
 
 // show page
 app.get('/game/:name', function(req, res) {
-    var nameOfTheGame = req.params.name;
-    var games = getGames();
-    var game = getGame(games, nameOfTheGame);
-
-    res.render('games-show', game);
+    db.game.findAll({
+        where: {
+            name: req.params.name
+        }
+    }).spread(function(game) {
+        res.render('games-show', { game: game });
+    });
 });
 
 app.get('/game/:name/edit', function(req, res) {
-    var nameOfTheGame = req.params.name;
-    var games = getGames();
-    var game = getGame(games, nameOfTheGame);
-
-    res.render('games-edit', game);
+    db.game.findAll({
+        where: {
+            name: req.params.name
+        }
+    }).spread(function(game) {
+        res.render('games-edit', { game: game });
+    });
 });
 
 app.put('/game/:name', function(req, res) {
-    var theNewGameData = req.body;
-
-    var nameOfTheGame = req.params.name;
-    var games = getGames();
-    var game = getGame(games, nameOfTheGame);
-
-    game.name = theNewGameData.name;
-    game.description = theNewGameData.description;
-
-    saveGames(games);
-
-    res.send(req.body);
+    db.game.update({
+        name: req.body.name,
+        description: req.body.description
+    }, {
+        where: {
+            name: req.body.name
+        }
+    }).spread(function(game) {
+        res.render('games-show', { game: game });
+    });
 });
 
 app.delete('/game/:name', function(req, res) {
-    var nameOfTheGame = req.params.name;
-    var games = getGames();
-    var game = getGame(games, nameOfTheGame);
-    var indexOfGameToDelete = games.indexOf(game);
-
-    games.splice(indexOfGameToDelete, 1);
-
-    saveGames(games);
-
-    res.send(game);
+    db.game.destroy({
+        where: {
+            name: req.params.name
+        }
+    }).then(function(game) {
+        res.render('games-show', { game: game });
+    });
 });
 
-// helper functions
-
-function getGame(games, nameOfTheGame) {
-    var game = null;
-
-    for (var i = 0; i < games.length; i++) {
-        if (games[i].name.toLowerCase() == nameOfTheGame.toLowerCase()) {
-            game = games[i];
-            break;
-        }
-    }
-
-    return game;
-}
-
-// Read list of games from file.
-function getGames() {
-    var fileContents = fs.readFileSync('./games.json'); // :'(
-    var games = JSON.parse(fileContents);
-    return games;
-}
-
-// Write list of games to file.
-function saveGames(games) {
-    fs.writeFileSync('./games.json', JSON.stringify(games));
-}
 
 // start the server
 
